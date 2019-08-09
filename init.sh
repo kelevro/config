766b2f07cfbb4fbe967f3e6b22db4fbcafac69fa
Anton Holovko <kelevro88@gmail.com>
2019-08-10 01:19:30 +0300

    update installer.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -s .tmux ~/.tmux
-ln -s .tmux.conf ~/.tmux.conf
+ln -s ~/.config/.tmux ~/.tmux
+ln -s ~/.config/.tmux.conf ~/.tmux.conf
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
